e5de353c7e325f3487a5a623beccf50cd8506d05
Baris Tuna <baristna@gmail.com>
2020-12-31 01:09:37 +0300

    intial commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,108 @@
+import { useState, useEffect } from 'react';
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
+import s from './index.scss'
+import shuffle from './shuffle';
+
+const defaultNumberStates = {}
+const defaultBag = []
+
+for (let i = 1; i <= 90; i++) {
+  defaultBag.push(i)
+  defaultNumberStates[i] = false
+}
 
 export default function Home() {
+  const [popHide, setPopHide] = useState(true);
+  const [bag, setBag] = useState(defaultBag);
+  const [lastNumber, setLastNumber] = useState(0);
+  const [drawList, setDrawList] = useState([]);
+  const [numberStates, setNumberStates] = useState(defaultNumberStates);
+
+  const drawNumber = () => {
+    if (!bag.length) { return }
+
+    if (bag.length + drawList.length !== 90) {
+      return
+    }
+
+    setPopHide(false);
+
+    setTimeout(() => {
+      setPopHide(true)
+    }, 1000)
+
+    const shuffled = shuffle(bag);
+    const popped = shuffled.pop()
+
+    const updatenumberStates = {...numberStates}
+    updatenumberStates[popped] = true;
+
+    const updateDrawList = [...drawList];
+    updateDrawList.unshift(popped)
+
+    const newBag = [...shuffled]
+
+    setBag(newBag)
+    setLastNumber(popped)
+    setDrawList(updateDrawList);
+    setNumberStates(updatenumberStates)
+
+    localStorage.setItem('bag', JSON.stringify(newBag))
+    localStorage.setItem('lastNumber', popped)
+    localStorage.setItem('drawList', JSON.stringify(updateDrawList))
+    localStorage.setItem('numberStates', JSON.stringify(updatenumberStates))
+  }
+
+  const reset = () => {
+    setBag(defaultBag)
+    setLastNumber(0)
+    setDrawList([]);
+    setNumberStates(defaultNumberStates)
+
+    localStorage.removeItem('bag')
+    localStorage.removeItem('lastNumber')
+    localStorage.removeItem('drawList')
+    localStorage.removeItem('numberStates')
+  }
+
+  useEffect(() => {
+    const savedBag = window.localStorage.getItem('bag')
+    const savedLastNumber = window.localStorage.getItem('lastNumber')
+    const savedDrawList = window.localStorage.getItem('drawList')
+    const savedNumberStates = window.localStorage.getItem('numberStates')
+  
+    savedBag && setBag(JSON.parse(savedBag));
+    savedLastNumber && setLastNumber(savedLastNumber);
+    savedDrawList && setDrawList(JSON.parse(savedDrawList));
+    savedNumberStates && setNumberStates(JSON.parse(savedNumberStates));
+  }, [])
+
   return (
     <div className={styles.container}>
+      <div className={`${s.pop} ${popHide ? s.hide : ''}`}>{lastNumber}</div>
       <Head>
-        <title>Create Next App</title>
+        <title>TOMBALA</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className={s.layout}>
+        <div className={s.occurs}>
+          {Object.entries(numberStates).map(([num, state]) => (
+            <div className={s.box}>
+              <div className={`${s.number} ${state ? s.drawed : ''} ${lastNumber == num ? s.lastnumber : ''}`}>{num}</div>
+            </div>
+          ))}
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <div className={s.drawlist}>
+          {drawList.map(num => <div className={s.drawlistitem}>{num}</div>)}
+        </div>
+      </div>
+      <div className={s.bar}>
+        <div className={`${s.btn} ${s.red}`} onClick={() => reset()}>RESET</div>
+        <div className={s.info}>Torba: {bag.length} | Çekilen: {drawList.length}</div>
+        <div className={s.divider}></div>
+        <div className={s.btn} onClick={() => drawNumber()}>NUMARA ÇEK</div>
+      </div>
     </div>
   )
 }
