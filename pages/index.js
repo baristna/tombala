66d5f96af7633aee00af5c302da91f6dd80122d2
Baris Tuna <baristna@gmail.com>
2020-12-31 19:28:55 +0300

    bank added

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'react';
-import Head from 'next/head'
-import styles from '../styles/Home.module.scss'
-import s from './index.scss'
+import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import styles from '../styles/Home.module.scss';
+import s from './index.scss';
 import shuffle from './shuffle';
 
 const defaultNumberStates = {}
@@ -12,7 +12,25 @@ for (let i = 1; i <= 90; i++) {
   defaultNumberStates[i] = false
 }
 
+const defaultBank = {
+  bank: 0,
+  c1: 5,
+  c2: 10,
+  bingo: 20,
+  people: [
+    { name: 'A', current: 25 },
+    { name: 'B', current: 25 },
+    { name: 'C', current: 25 },
+    { name: 'D', current: 25 },
+    { name: 'E', current: 25 },
+    { name: 'F', current: 25 },
+    { name: 'G', current: 25 },
+  ]
+}
+
 export default function Home() {
+  const [bankState, setBankState] = useState(defaultBank)
+  const [showBank, setShowBank] = useState(false)
   const [popHide, setPopHide] = useState(true);
   const [bag, setBag] = useState(defaultBag);
   const [lastNumber, setLastNumber] = useState(0);
@@ -68,19 +86,63 @@ export default function Home() {
 
   useEffect(() => {
     const savedBag = window.localStorage.getItem('bag')
+    const savedBankState = window.localStorage.getItem('bankState')
     const savedLastNumber = window.localStorage.getItem('lastNumber')
     const savedDrawList = window.localStorage.getItem('drawList')
     const savedNumberStates = window.localStorage.getItem('numberStates')
   
     savedBag && setBag(JSON.parse(savedBag));
+    savedBankState && setBankState(JSON.parse(savedBankState));
     savedLastNumber && setLastNumber(savedLastNumber);
     savedDrawList && setDrawList(JSON.parse(savedDrawList));
     savedNumberStates && setNumberStates(JSON.parse(savedNumberStates));
   }, [])
 
+  const pay = (name, amount) => {
+    const newBankState = {...bankState}
+
+    newBankState.bank -= amount
+    newBankState.people[newBankState.people.findIndex(i => i.name == name)].current += amount 
+
+    setBankState(newBankState)
+    localStorage.setItem('bankState', JSON.stringify(newBankState))
+  }
+
+  const bankReset = () => {
+    setBankState(defaultBank)
+    localStorage.removeItem('bankState')
+  }
+
   return (
     <div className={styles.container}>
       <div className={`${s.pop} ${popHide ? s.hide : ''}`}>{lastNumber}</div>
+      {showBank ? (
+        <div className={`${s.bank}`}>
+          <div className={s.bankclose} onClick={() => setShowBank(false)}>X</div>
+          <div className={s.bankbox}>
+            <div className={s.people}>
+              {bankState.people.map((p) => (
+                <div className={s.person}>
+                  <div className={s.pname}>{p.name}</div>
+                  <div className={`${s.ppay} ${s.red}`} onClick={() => { pay(p.name, -5)}}>ÖDE</div>
+                  <div className={`${s.ppay} ${s.yellow}`} onClick={() => { pay(p.name, bankState.c1)}}>1.Ç</div>
+                  <div className={`${s.ppay} ${s.yellow}`} onClick={() => { pay(p.name, bankState.c2)}}>2.Ç</div>
+                  <div className={`${s.ppay} ${s.green}`} onClick={() => { pay(p.name, bankState.bingo)}}>TOMBALA</div>
+                  <div className={p.personbank}>{p.current}TL</div>
+                </div>
+              ))}
+            </div>
+            <div className={s.bingobank}>
+              <div className={s.bingototal}>Toplam {bankState.bank}TL</div>
+              <div className={s.bingoorder}>1. Çinko = 5TL</div>
+              <div className={s.bingoorder}>2. Çinko = 10TL</div>
+              <div className={s.bingoorder}>3. Çinko = 20TL</div>
+              <div className={s.divider}></div>
+              <div className={s.bingobankreset} onClick={() => bankReset()}>RESET</div>
+            </div>
+          </div>
+        </div>
+      ) : null}
       <Head>
         <title>TOMBALA</title>
         <link rel="icon" href="/favicon.ico" />
@@ -101,7 +163,8 @@ export default function Home() {
         <div className={`${s.btn} ${s.red}`} onClick={() => reset()}>RESET</div>
         <div className={s.info}>Torba: {bag.length} | Çekilen: {drawList.length}</div>
         <div className={s.divider}></div>
-        <div className={s.btn} onClick={() => drawNumber()}>NUMARA ÇEK</div>
+        <div className={s.btn} style={{ marginRight: '20px' }} onClick={() => drawNumber()}>NUMARA ÇEK</div>
+        <div className={`${s.btn} ${s.green}`} onClick={() => setShowBank(!showBank)}>BANK</div>
       </div>
     </div>
   )
